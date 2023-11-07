b98afcb1cf7afc65f99809b72d14bea9bde2a888
Ziyu (Ashley) Ren <92663054+AshleyZR@users.noreply.github.com>
2023-11-07 03:00:37 +0000

    finished db project

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -2,4 +2,46 @@ class MiscController < ApplicationController
   def homepage
     render({ :template => "misc_templates/home"})
   end
+  
+  def directors_page
+    render({:template => "misc_templates/directors_page"})
+  end
+
+  def director_page
+    director_id = params.fetch("director_id")
+    @profile = Director.where({:id => director_id}).first
+    render({:template => "misc_templates/director_page"})
+  end
+
+  def movies_page
+    render({:template => "misc_templates/movies_page"})
+  end
+
+  def movie_page
+    movie_id = params.fetch("movie_id")
+    @profile = Movie.where({:id => movie_id}).first
+    render({:template => "misc_templates/movie_page"})
+  end
+
+  def actors_page
+    render({:template => "misc_templates/actors_page"})
+  end
+
+  def actor_page
+    actor_id = params.fetch("actor_id")
+    @profile = Actor.where({:id => actor_id}).first
+    render({:template => "misc_templates/actor_page"})
+  end
+
+  def eldest_page
+    valid_dob = Director.where.not({ :dob => nil })
+    @profile = Director.all.order(:dob => :desc).first
+    render({:template => "misc_templates/eldest_page"})
+  end 
+
+  def youngest_page
+    valid_dob = Director.where.not({:dob => nil })
+    @profile = valid_dob.all.order(:dob => :asc).first
+    render({:template => "misc_templates/youngest_page"})
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+  get('/directors', {:controller => "misc", :action => "directors_page"})
+  get("/directors/youngest", {:controller => "misc", :action => "youngest_page"})
+  get("/directors/eldest", {:controller => "misc", :action => "eldest_page"})
+  get("/directors/:director_id",{:controller => "misc", :action => "director_page"})
+  get("/movies", { :controller => "misc", :action => "movies_page" })
+  get("/movies/:movie_id", { :controller => "misc", :action => "movie_page" })
+  get("/actors", { :controller => "misc", :action => "actors_page" })
+  get("/actors/:actor_id", { :controller => "misc", :action => "actor_page" })
 end
